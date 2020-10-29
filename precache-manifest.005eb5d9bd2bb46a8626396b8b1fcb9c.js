29c2804458c31f7135a8f6a45bdac726f90e8b87
Luis Javier Lozoya <luisloart@gmail.com>
2020-10-28 21:18:41 -0400

    Updates

---
+++ precache-manifest.005eb5d9bd2bb46a8626396b8b1fcb9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe8612f9f0ac93d137f2e7aa677ddf0",
+    "url": "/WeatherCheck-API/index.html"
+  },
+  {
+    "revision": "af6ab18b06f1253cab86",
+    "url": "/WeatherCheck-API/static/css/main.1b4ad1f5.chunk.css"
+  },
+  {
+    "revision": "558af160ae18c8834c1b",
+    "url": "/WeatherCheck-API/static/js/2.bf98a0d9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/WeatherCheck-API/static/js/2.bf98a0d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af6ab18b06f1253cab86",
+    "url": "/WeatherCheck-API/static/js/main.74ee57f9.chunk.js"
+  },
+  {
+    "revision": "e50951a3864a92513fe9",
+    "url": "/WeatherCheck-API/static/js/runtime-main.21d2764a.js"
+  }
+]);
